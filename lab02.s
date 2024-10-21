cb0d23f5c93168c4781e0232ea0f29d3d7380b00
Vasilis Papachristou <vasilispapach1@gmail.com>
2024-10-21 17:51:05 +0300

    Completed lab02

--- lab02.s
+++ lab02.s
@@ -10,8 +10,18 @@ array: .word 1, 0, 1, 12, 0, 1, 4
     li a2, 1
 prog:
 #-----------------------------
-# Write your code here!
-# Do not remove the prog label or write code above it!
+findLast_forwards_withPointers:
+    add s0, zero, zero
+loop:
+    beq a1, zero, done
+    lw t1, 0(a0)
+    bne t1, a2, next
+    add s0, a0, zero
+    j loop
+next:
+    addi a0, a0, 4
+    addi a1, a1, -1
+    
 #-----------------------------
 done:
     addi a7, zero, 10 
